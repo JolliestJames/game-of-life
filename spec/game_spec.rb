7841a969dbef74266c29084f20f22ec3ca6755aa
ToBeAmped <martinja@oregonstate.edu>
2018-06-11 14:38:50 -0700

    Make 'is instantiated with a grid' fail

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,14 @@
 require_relative '../lib/game'
 
 describe "Conway's game of life" do
+  let (:game) { Game.new }
 
   it 'exists' do
     Game.new
   end
 
-  xit 'is instantiated with a grid' do
-
+  it 'is instantiated with a grid' do
+    expect(game.grid).not_to be_nil
   end
 
   xit 'provides a way to obtain the grid it was instantiated with' do
