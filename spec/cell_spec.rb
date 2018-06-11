53e7b67fd651e89c86620ce71fe15d8f47ea7fad
ToBeAmped <martinja@oregonstate.edu>
2018-06-11 14:20:16 -0700

    Make it 'can die' test fail

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -11,8 +11,9 @@ describe "A cell" do
     expect(cell).to be_alive
   end
 
-  xit 'can die' do
-
+  it 'can die' do
+    cell.die
+    expect(cell).to be_dead
   end
 
   xit 'has neighbors, which is initially an empty array' do
